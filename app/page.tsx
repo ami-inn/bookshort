681d5f1eff544251a88c5552a3cf42842031ef18
ami-inn <amipk2001@gmail.com>
2026-04-09 23:55:00 +0530

    update: book card added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+import BookCard from '@/components/BookCard'
 import HeroSection from '@/components/HeroSection'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+
+const books = [
+    {
+        _id: "1",
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        coverURL: "/assets/great-gatsby-cover.png",
+        slug: "the-great-gatsby"
+    },
+]
 
 const page = () => {
   return (
     <main className='wrapper container'>
     <HeroSection />
+
+      <div className="library-books-grid">
+                {books.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+                ))}
+            </div>
+        
     </main>
   )
 }
